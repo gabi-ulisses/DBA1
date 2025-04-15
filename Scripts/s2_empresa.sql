1a62bbf7ac924af10e3e7f8c76851f6a129d06fb
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2025-04-14 21:34:48 -0300

    Update s2_empresa.sql

--- Scripts/s2_empresa.sql
+++ Scripts/s2_empresa.sql
@@ -5,12 +5,11 @@
 USE EMPRESA;
 
 -- 1. Listar o nome, sexo, data de nascimento e endereço completo dos funcionários.
-SELECT NOME, SEXO, DATANASC, 
-       CONCAT(RUA, ', ', NUMERO, ' ', IFNULL(COMPL, ''), ', ', BAIRRO, ', ', CIDADE, '-', ESTADO, ', CEP: ', CEP) AS ENDERECO_COMPLETO
+SELECT NOME, SEXO, DATANASC 'DATA DE NASCIMENTO', RUA, NUMERO, COMPL, BAIRRO, CIDADE, ESTADO, CEP
 FROM FUNCIONARIO;
 
 -- 2. Listar o nome, data de nascimento e salário das funcionárias, ordenado por salário em ordem decrescente.
-SELECT NOME, DATANASC, SALARIO 
+SELECT NOME, DATANASC AS 'DATA DE NASCIMENTO', SALARIO 
 FROM FUNCIONARIO 
 WHERE SEXO = 'F' 
 ORDER BY SALARIO DESC;
@@ -20,35 +19,32 @@ SELECT DISTINCT PROJLOCAL AS 'LOCAIS'
 FROM PROJETO;
 
 -- 4. Listar nome, data de nascimento e salário dos funcionários que não trabalham no departamento 4.
-SELECT NOME, DATANASC, SALARIO 
+SELECT NOME, DATANASC AS 'DATA DE NASCIMENTO', SALARIO 
 FROM FUNCIONARIO 
 WHERE DNUMERO <> 4;
 
 -- 5. Listar nome e data de nascimento das funcionárias que ganham menos de R$5.000,00.
-SELECT NOME, DATANASC 
+SELECT NOME, DATANASC AS 'DATA DE NASCIMENTO'
 FROM FUNCIONARIO 
 WHERE SALARIO < 5000 AND SEXO = 'F';
 
 -- 6. Listar nome, data de nascimento e endereço completo dos funcionários cujo nome começa com a letra A.
-SELECT NOME, DATANASC, 
-       CONCAT(RUA, ', ', NUMERO, ' ', IFNULL(COMPL, ''), ', ', BAIRRO, ', ', CIDADE, '-', ESTADO, ', CEP: ', CEP) AS ENDERECO_COMPLETO
+SELECT NOME, DATANASC AS 'DATA DE NASCIMENTO', NOME, SEXO, RUA, NUMERO, COMPL, BAIRRO, CIDADE, ESTADO, CEP
 FROM FUNCIONARIO 
 WHERE NOME LIKE 'A%';
 
 -- 7. Listar nome e endereço completo dos funcionários que moram em avenidas.
-SELECT NOME, 
-       CONCAT(RUA, ', ', NUMERO, ' ', IFNULL(COMPL, ''), ', ', BAIRRO, ', ', CIDADE, '-', ESTADO, ', CEP: ', CEP) AS ENDERECO_COMPLETO
+SELECT NOME, RUA, NUMERO, COMPL, BAIRRO, CIDADE, ESTADO, CEP
 FROM FUNCIONARIO 
 WHERE RUA LIKE 'Av.%';
 
 -- 8. Listar nome, sexo e data de nascimento dos funcionários que nasceram na década de 60.
-SELECT NOME, SEXO, DATANASC 
+SELECT NOME, SEXO, DATANASC AS 'DATA DE NASCIMENTO'
 FROM FUNCIONARIO 
 WHERE DATANASC BETWEEN '1960-01-01' AND '1969-12-31';
 
 -- 9. Listar nome, salário atual e o salário com reajuste de 5% para todos os funcionários.
-SELECT NOME, SALARIO, 
-       ROUND(SALARIO * 1.05, 2) AS SALARIO_REAJUSTADO 
+SELECT NOME, SALARIO, SALARIO *1.05 AS SALARIO_REAJUSTADO 
 FROM FUNCIONARIO;
 
 -- 10. Listar os CPFs dos funcionários que trabalham mais de 10 horas no projeto de número 3.
@@ -72,39 +68,43 @@ SELECT MIN(HORAS) AS MENOR_CARGA,
 FROM ATRIBUICAO;
 
 -- 14. Listar a soma dos salários dos funcionários que trabalham no departamento 4.
-SELECT SUM(SALARIO) AS SOMA_SALARIOS 
+SELECT SUM(SALARIO) AS 'SOMA DE SALÁRIOS'
 FROM FUNCIONARIO 
 WHERE DNUMERO = 4;
 
 -- 15. Listar a média salarial dos funcionários por departamento.
-SELECT DNUMERO, 
-       ROUND(AVG(SALARIO), 2) AS MEDIA_SALARIO 
+SELECT DNUMERO AS 'Nº DEPARTAMENTO', 
+       ROUND(AVG(SALARIO), 2) AS 'MÉDIA SALÁRIOS'
 FROM FUNCIONARIO 
 GROUP BY DNUMERO;
 
 -- 16. Listar a quantidade de projetos que o funcionário de CPF 1234 atua.
-SELECT COUNT(*) AS QTD_PROJETOS 
+SELECT COUNT(*) AS 'QUANTIDADE DE PROJETOS'
 FROM ATRIBUICAO 
 WHERE CPF = '1234';
 
 -- 17. Listar a quantidade de projetos que cada funcionário atua.
 SELECT CPF, 
-       COUNT(*) AS QTD_PROJETOS 
+       COUNT(*) AS 'QUANTIDADE DE PROJETOS' 
 FROM ATRIBUICAO 
 GROUP BY CPF;
 
 -- 18. Listar a média salarial dos funcionários que moram em São Paulo.
-SELECT ROUND(AVG(SALARIO), 2) AS MEDIA_SALARIO_SP 
+SELECT ROUND(AVG(SALARIO), 2) AS 'MÉDIA SALARIAL SP'
 FROM FUNCIONARIO 
 WHERE CIDADE = 'São Paulo';
 
 -- 19. Listar a média salarial dos funcionários por cidade.
 SELECT CIDADE, 
-       ROUND(AVG(SALARIO), 2) AS MEDIA_SALARIO 
+       ROUND(AVG(SALARIO), 2) AS 'MÉDIA SALARIAL'
 FROM FUNCIONARIO 
 GROUP BY CIDADE;
 
 -- 20. Listar o nome e a idade dos funcionários.
 SELECT NOME, 
-       TIMESTAMPDIFF(YEAR, DATANASC, CURDATE()) AS IDADE 
+       TIMESTAMPDIFF(YEAR, DATANASC, CURDATE()) AS IDADE
+FROM FUNCIONARIO;
+
+SELECT NOME, DATANASC AS 'DATA DE NASCIMENTO',
+       YEAR(sysdate()) - YEAR(DATANASC) AS IDADE 
 FROM FUNCIONARIO;
